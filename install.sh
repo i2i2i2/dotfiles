08557c4c3b0bd146fa13b62737a7aa6506b807b5
Chenxu Jiang <chj028@ucsd.edu>
2019-01-11 10:43:16 -0800

    retab

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ do_install_pkg() {
 	# perform some very rudimentary platform detection
 	lsb_dist=$( get_distribution )
 	lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
-  lsb_ver=$( get_version )
+	lsb_ver=$( get_version )
 
 	# Run setup for each distro accordingly
 	case "$lsb_dist" in
@@ -57,63 +57,63 @@ do_install_pkg() {
 			(
 				set -x
 				$sh_c 'apt-get update -qq >/dev/null'
-        for pkg in $pkgs; do
-				  $sh_c "apt-get install -y -qq $pkg >/dev/null"
-        done
+				for pkg in $pkgs; do
+					$sh_c "apt-get install -y -qq $pkg >/dev/null"
+				done
 			)
 			;;
-    fedora)
+		fedora)
 			pkgs="coreutils wget curl tmux vim mosh fish git psmisc findutils tree gcc golang python2 python3 nodejs iproute nmap-ncat tcpdump net-tools traceroute iptables iputils"
 			(
 				set -x
-        for pkg in $pkgs; do
-				  $sh_c "dnf install -y -q $pkg"
-        done
+				for pkg in $pkgs; do
+					$sh_c "dnf install -y -q $pkg"
+				done
 			)
-      ;;
+			;;
 		*) # assume rhel or centos
-      url="https://dl.fedoraproject.org/pub/epel/epel-release-latest-$lsb_ver.noarch.rpm"
+			url="https://dl.fedoraproject.org/pub/epel/epel-release-latest-$lsb_ver.noarch.rpm"
 			pkgs="yum-utils coreutils vim-enhanced which wget curl tmux mosh fish git psmisc tree gcc golang python nodejs iproute nmap-ncat tcpdump net-tools traceroute iptables iputils"
-      (
+			(
 				set -x
-        if ! yum list installed epel-release ; then
-          $sh_c "yum install -y -q $url"
-        fi
-        for pkg in $pkgs; do
-				  $sh_c "yum install -y -q $pkg"
-        done
+				if ! yum list installed epel-release ; then
+					$sh_c "yum install -y -q $url"
+				fi
+				for pkg in $pkgs; do
+					$sh_c "yum install -y -q $pkg"
+				done
 			)
 			;;
 	esac
 }
 
 do_install_dotfiles() {
-  hash=$(date +%s | sha256sum | head -c 8)
-  installDir="/tmp/myworkspace_$hash"
-  echo $installDir
-  mkdir -p $installDir
-  if ! git clone "https://github.com/i2i2i2/dotfiles" $installDir; then
-    exit 1
-  fi
+	hash=$(date +%s | sha256sum | head -c 8)
+	installDir="/tmp/myworkspace_$hash"
+	echo $installDir
+	mkdir -p $installDir
+	if ! git clone "https://github.com/i2i2i2/dotfiles" $installDir; then
+		exit 1
+	fi
 
-  backupOpt="--backup=simple --suffix=.mybackup"
-  (
-    set -x
-    cd $installDir/src
-    for dir in `find . -type d`; do
-      install -m 755 -d $HOME/$dir;
-      for file in `find $dir -maxdepth 1 -type f`; do
-        install -m 644 $backupOpt $file $HOME/$dir
-      done
-    done;
-  )
-  rm -rf $installDir
-  exit 0
+	backupOpt="--backup=simple --suffix=.mybackup"
+	(
+		set -x
+		cd $installDir/src
+		for dir in `find . -type d`; do
+			install -m 755 -d $HOME/$dir;
+			for file in `find $dir -maxdepth 1 -type f`; do
+				install -m 644 $backupOpt $file $HOME/$dir
+			done
+		done;
+	)
+	rm -rf $installDir
+	exit 0
 }
 
 do_install() {
-  do_install_pkg
-  do_install_dotfiles
+	do_install_pkg
+	do_install_dotfiles
 }
 
 # wrapped up in a function so that we have some protection against only getting
