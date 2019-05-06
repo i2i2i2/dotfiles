bc957d4fb4cdfc33913b1aeb4b01272ea841c375
i2i2i2 <chj028@ucsd.edu>
2019-05-05 23:09:58 -0700

    remove pl installs

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ do_install_pkg() {
 	# Run setup for each distro accordingly
 	case "$lsb_dist" in
 		ubuntu|debian|raspbian)
-			pkgs="apt-utils coreutils wget curl tmux vim mosh fish git psmisc tree g++ golang python python3 nodejs iproute2 netcat tcpdump net-tools traceroute iptables iputils-ping exuberant-ctags"
+			pkgs="apt-utils coreutils wget curl tmux vim fish git psmisc tree iproute2 netcat tcpdump net-tools traceroute iptables iputils-ping exuberant-ctags"
 			(
 				set -x
 				$sh_c 'apt-get update -qq >/dev/null'
@@ -63,7 +63,7 @@ do_install_pkg() {
 			)
 			;;
 		fedora)
-			pkgs="coreutils wget curl tmux vim mosh fish git psmisc findutils tree gcc golang python2 python3 nodejs iproute nmap-ncat tcpdump net-tools traceroute iptables iputils ctags"
+			pkgs="coreutils wget curl tmux vim fish git psmisc findutils tree proute nmap-ncat tcpdump net-tools traceroute iptables iputils ctags"
 			(
 				set -x
 				for pkg in $pkgs; do
@@ -73,7 +73,7 @@ do_install_pkg() {
 			;;
 		*) # assume rhel or centos
 			url="https://dl.fedoraproject.org/pub/epel/epel-release-latest-$lsb_ver.noarch.rpm"
-			pkgs="yum-utils coreutils vim-enhanced which wget curl tmux mosh fish git psmisc tree gcc golang python nodejs iproute nmap-ncat tcpdump net-tools traceroute iptables iputils ctags"
+			pkgs="yum-utils coreutils vim-enhanced which wget curl tmux fish git psmisc tree iproute nmap-ncat tcpdump net-tools traceroute iptables iputils ctags"
 			(
 				set -x
 				if ! yum list installed epel-release ; then
